469f68091c59ce33383fbc6c9c2800482788f933
marek-sz <marekszymkowiak1@gmail.com>
2021-06-25 13:22:35 +0200

    test reporter and test info

--- src/test/java/MathUtils/MathUtilsTest.java
+++ src/test/java/MathUtils/MathUtilsTest.java
@@ -8,16 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("All tests for MathUtil class")
 class MathUtilsTest {
+    private TestInfo testInfo;
+    private TestReporter testReporter;
     private MathUtils mathUtils;
 
     @BeforeEach
-    void init() {
+    void init(TestInfo testInfo, TestReporter testReporter) {
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
         mathUtils = new MathUtils();
-    }
-
-    @AfterEach
-    void cleanUp() {
-        System.out.println("cleaning up...");
     }
 
     @Test
@@ -77,4 +76,30 @@ class MathUtilsTest {
         }
     }
 
+    // for optimisation write lambda expression to create a message. Will be executed only if test fails
+    @Test
+    @DisplayName("Message passed only when test fails")
+    void testComputeCircleArena() {
+        double expected = 314.1592653589793;
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), () -> "Test should return " + expected);
+        assertEquals(0, mathUtils.computeCircleArea(10), () -> "Test should return " + expected);
+    }
+
+    @RepeatedTest(3)
+    void repeatedTest() {
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10));
+    }
+
+    @RepeatedTest(3)
+    void repeatedTestWhenYouWantToTakeControlOfRepetitions(RepetitionInfo repetitionInfo) {
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10));
+    }
+
+    //before that you have to add parameter to method (BeforeAll e.g)
+    @Test
+    @DisplayName("test info and test tags")
+    void showLogsOfTheTest() {
+        testReporter.publishEntry(" " + testInfo.getDisplayName() + testInfo.getTags());
+
+    }
 }
